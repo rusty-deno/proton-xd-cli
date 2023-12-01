dfbf5ca8c74305b4bddc009df61089e3bc928359
kakashi-69-xd <trishiragolder69@outlook.com>
2023-12-02 02:14:43 +0530

    implemented build

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 mod ser;
+use std::path::PathBuf;
+
 use ser::*;
 
 
@@ -8,11 +10,31 @@ use tokio::*;
 #[main]
 async fn main() {
   let args=Operation::new();
+  
+  println!("{:?}",&args);
 
-  println!("{args:?}")
-
-
+  match args {
+    Operation::Build { dir }=> build(dir).await.unwrap(),
+    _=> todo!()
+  }
 }
 
+async fn build(dir: PathBuf)-> io::Result<()> {
+  ensure_dir(&dir).await?;
+
+  let mut process=process::Command::new("deno");
+  process.arg("compile")
+  .args(["--no-prompt","-o",dir.join("xd").to_str().unwrap(),])
+  .arg("./proton-xd-src/main.ts");
+
+  std::process::exit(process.spawn()?.wait().await?.code().unwrap_or_default())
+}
+
+async fn ensure_dir(path: &PathBuf)-> io::Result<()> {
+  if fs::try_exists(path).await? {
+    return Ok(());
+  }
+  fs::create_dir(path).await
+}
 
 
--- src/ser.rs
+++ src/ser.rs
@@ -1,10 +1,33 @@
+use std::path::PathBuf;
 use clap::Parser;
 
 
 #[derive(Parser,Debug)]
 #[command(author,version,about,long_about=None)]
 pub enum Operation {
-  Build(Build)
+  Build {
+    #[arg(long="dir",default_value="build")]
+    dir: PathBuf,
+  },
+  New {
+    path: Box<str>,
+    #[arg(short='t',long="template")]
+    template: Option<Box<str>>,
+    #[arg(long="ts")]
+    ts: bool,
+    #[arg(long="js")]
+    js: bool
+  },
+  Init {
+    #[arg(default_value="./")]
+    path: PathBuf,
+    #[arg(short='t',long="template")]
+    template: Option<Box<str>>,
+    #[arg(long="ts")]
+    ts: bool,
+    #[arg(long="js")]
+    js: bool
+  }
 }
 
 impl Operation {
@@ -15,11 +38,17 @@ impl Operation {
 
 
 
-#[derive(Parser,Debug)]
-pub struct Build {
-  #[arg(short='r')]
-  release: bool,
-  #[arg(short='p')]
-  path: String,
-}
+
+
+
+// match value.as_ref() {
+//   "next"=> Next,
+//   "react"=> React,
+//   "remix"=> Remix,
+//   "ruck"=> Ruck,
+//   "svelte"=> Svelte,
+//   "sanilla"=> Vanilla,
+//   "vue"=> Vue,
+//   val=> panic!("{val} is not a valid template")
+// }
 
