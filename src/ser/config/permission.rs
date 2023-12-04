f87ce176dd4c3b8183082c21df74e70a5d5686ef
kakashi-69-xd <trishiragolder69@outlook.com>
2023-12-04 20:15:42 +0530

    impl Default for Permissions

--- src/ser/config/permission.rs
+++ src/ser/config/permission.rs
@@ -1,6 +1,12 @@
 use serde::Deserialize;
-use super::Val;
 
+use super::{
+  Value::Vec,
+  Val
+};
+
+
+pub(crate) const FFI_DIR: &str="https://github.com/kakashi-69-xd/proton-xd/bindings/bin";
 
 
 #[derive(Deserialize,Debug)]
@@ -32,7 +38,31 @@ pub(crate) enum Permissions {
 
 impl Permissions {
   pub fn new()-> Self {
-    unimplemented!()
+    Self::default()
+  }
+}
+
+impl Default for Permissions {
+  fn default()-> Self {
+    Self::Permissions {
+      //allow
+      allow_read: None,
+      allow_write: None,
+      allow_net: None,
+      unsafely_ignore_certificate_errors: None,
+      allow_env: None,
+      allow_sys: None,
+      allow_ffi: Some(Vec([FFI_DIR.into()].into())),// path of dylib
+      allow_hrtm: None,
+      //deny
+      deny_read: None,
+      deny_write: None,
+      deny_net: None,
+      deny_env: None,
+      deny_sys: None,
+      deny_ffi: None,
+      deny_hrtm: None,
+    }
   }
 }
 
