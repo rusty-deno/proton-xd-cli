5c9467905aa58f27d27f5564478f943f7378f872
kakashi-69-xd <trishiragolder69@outlook.com>
2023-12-04 15:36:09 +0530

    implemented fetch config

--- src/ser/config/mod.rs
+++ src/ser/config/mod.rs
@@ -3,13 +3,16 @@ mod unstable;
 mod permission;
 mod compiler_options;
 
+
 use unstable::*;
 use permission::*;
 use compiler_options::*;
 
 
+use tokio::*;
+use std::env;
 use serde::Deserialize;
-
+use std::path::PathBuf;
 
 
 
@@ -34,15 +37,39 @@ pub(crate) struct Config {
   unstable: Unstable
 }
 
+impl Config {
+  pub async fn fetch_config()-> io::Result<(Config,PathBuf)> {
+    loop {
+      let res=fs::read_to_string("./proton-config.json").await;
 
-
-
-
-
-
-#[test]
-fn xd() {
-  let config: Config=serde_json::from_str(&std::fs::read_to_string("./proton-config.json").unwrap()).unwrap();
-  println!("{config:?}")
+      if let Ok(res)=res {
+        return Ok((serde_json::from_str(&res).unwrap(),env::current_dir()?));
+      }
+      
+      match res.unwrap_err().kind() {
+        io::ErrorKind::NotFound=> env::set_current_dir("..")?,
+        _=> panic!("No `proton-xd-config.json` file found!")
+      }
+    }
+  }
 }
 
+
+
+
+
+
+
+
+
+
+#[cfg(test)]
+mod tests {
+  use crate::ser::config::Config;
+
+  #[test]
+  fn xd() {
+    let config: Config=serde_json::from_str(&std::fs::read_to_string("./proton-config.json").unwrap()).unwrap();
+    println!("{config:?}")
+  }
+}
