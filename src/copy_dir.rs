6222798ca23a9b66381fd342672c8e26a9b0d70f
kakashi-69-xd <trishiragolder69@outlook.com>
2023-12-08 00:26:33 +0530

    "I WROTE COMMENTS"

--- src/copy_dir.rs
+++ src/copy_dir.rs
@@ -9,7 +9,7 @@ use std::{
 
 
 
-/// copies a directory to specified `dest` path recursively. (actually iteratively)
+/// Copies a directory to specified `dest` path recursively. (actually iteratively)
 /// 
 /// `exceptions` is actually a regex expression that decides whether a file/directory should be copied.
 /// # Example
@@ -22,10 +22,12 @@ pub async fn copy_dir_all<F: AsRef<Path>,T: AsRef<Path>>(from: F,to: T,exception
   let mut queue=LinkedList::from_iter([(from.as_ref().to_owned(),to.as_ref().to_owned())]);
 
   while let Some((src,dest))=queue.pop_front() {
+    //ensuring destination path
     fs::create_dir_all(&dest).await?;
     let mut iter=fs::read_dir(src).await?;
 
     while let Some(entry)=iter.next_entry().await? {
+      // skipping exceptions.
       if except.is_match(entry.path().to_str().unwrap_or_default()) {
         continue;
       }
@@ -33,6 +35,7 @@ pub async fn copy_dir_all<F: AsRef<Path>,T: AsRef<Path>>(from: F,to: T,exception
       let entry_type=entry.file_type().await?;
       let entry_dest_path=dest.join(entry.file_name());
 
+      // NOTE: It doesn't care about symlinks.
       match entry_type.is_file() {
         true=> {
           tokio::spawn(fs::copy(entry.path(),entry_dest_path)).await??;
@@ -50,10 +53,20 @@ pub async fn copy_dir_all<F: AsRef<Path>,T: AsRef<Path>>(from: F,to: T,exception
 
 #[cfg(test)]
 mod tests {
-  use tokio::test;
+  use tokio::{test, fs};
   #[test]
   async fn xd() {
+    fs::create_dir_all("./test/xd").await.unwrap();
+
     super::copy_dir_all("./test/repo","./test/xd",".git*").await.unwrap()
+  }
+
+  #[test]
+  async fn resource() {
+    let temp=std::env::temp_dir();
+    fs::write(temp.join("./xd.ts"),"console.log(\"xd\");").await.unwrap();
+
+    println!("{}",temp.display())
   }
 }
 
